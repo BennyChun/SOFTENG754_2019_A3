9e3923c8ff6a7765ed42f9254a8314038028a74a
Benny Chun <bennychun97@gmail.com>
2019-04-05 11:13:31 +1300

    test added

--- src/test/java/nz/ac/auckland/SOFTENG754_2019_A3/DollarTest.java
+++ src/test/java/nz/ac/auckland/SOFTENG754_2019_A3/DollarTest.java
@@ -1,4 +1,5 @@
 package nz.ac.auckland.SOFTENG754_2019_A3;
+
 import static org.junit.Assert.*;
 import org.bson.Document;
 import org.junit.Test;
@@ -7,6 +8,7 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 public class DollarTest {
+
 	@Test
 	public void testDollarDivision() {
 		// Given
@@ -18,15 +20,15 @@ public class DollarTest {
 		// Then
 		assertEquals(new Dollar(5), result);
 		// You could change 5 to 4 to see the build failure
-		}
+	}
 
-		@Test(expected=ArithmeticException.class)
-		public void testDollarDividedByZero() {
-			// Given
-			Dollar ten = new Dollar(10);
 
-			// When
-			ten.dividedBy(0);
-		}
+	@Test(expected=ArithmeticException.class)
+	public void testDollarDividedByZero() {
+		// Given
+		Dollar ten = new Dollar(10);
+
+		// When
+		ten.dividedBy(0);
 	}
 }
